06fa6362c0549ace036b8c9885b60ce908228baf
ro7m <ro7m@users.noreply.github.com>
2024-11-01 15:12:22 +0000

    deploy: 8b24339ea4a4f17f5e8c4053c32fd8320e90ddd8

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
+  }
+})
